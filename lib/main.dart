374182ef8fbfa6ff3ea2afdc430779018ee50998
Wojtas399 <wojtekp26@gmail.com>
2024-04-14 12:37:19 +0200

    Added mapbox_search package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:mapbox_search/mapbox_search.dart';
 import 'package:motorbike_navigator/app.dart';
 import 'package:motorbike_navigator/env.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MapboxOptions.setAccessToken(Env.mapboxAccessToken);
+  MapBoxSearch.init(Env.mapboxAccessToken);
   runApp(
     const ProviderScope(
       child: App(),
