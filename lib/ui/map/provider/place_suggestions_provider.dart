34bbba923de580016b1cf7de0e60ce5fc887118a
Wojtas399 <wojtekp26@gmail.com>
2024-04-15 11:33:11 +0200

    Created place suggestions provider for map feature.

---
+++ lib/ui/map/provider/place_suggestions_provider.dart
@@ -0,0 +1,24 @@
+import 'package:motorbike_navigator/entity/place_suggestion.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../data/repository/place/place_suggestion_repository_method_providers.dart';
+
+part 'place_suggestions_provider.g.dart';
+
+@riverpod
+class PlaceSuggestions extends _$PlaceSuggestions {
+  @override
+  Future<List<PlaceSuggestion>> build() async => [];
+
+  Future<void> search(String query) async {
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(
+      () => ref.watch(
+        searchPlacesProvider(
+          query: query,
+          limit: 10,
+        ).future,
+      ),
+    );
+  }
+}
