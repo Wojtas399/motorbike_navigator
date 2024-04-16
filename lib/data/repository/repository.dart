d2bbb77d54e2011a430d99bdc64de177d396dbd6
Wojtas399 <wojtekp26@gmail.com>
2024-04-16 08:56:22 +0200

    Created repository model.

---
+++ lib/data/repository/repository.dart
@@ -0,0 +1,30 @@
+import 'package:collection/collection.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../../entity/entity.dart';
+
+abstract class Repository<T extends Entity> {
+  final BehaviorSubject<List<T>> _repositoryState$ =
+      BehaviorSubject<List<T>>.seeded([]);
+
+  Repository({List<T> initialData = const []}) {
+    _repositoryState$.add(initialData);
+  }
+
+  Stream<List<T>?> get repositoryState$ => _repositoryState$.stream;
+
+  bool get isRepositoryStateEmpty => _repositoryState$.value.isEmpty == true;
+
+  void addEntity(T entity) {
+    final bool doesEntityExist = _repositoryState$.value.firstWhereOrNull(
+          (element) => element.id == entity.id,
+        ) !=
+        null;
+    if (doesEntityExist) {
+      throw '[Repository] Entity $entity already exists in repository state';
+    }
+    final List<T> entities = [..._repositoryState$.value];
+    entities.add(entity);
+    _repositoryState$.add(entities);
+  }
+}
---
+++ test/data/repository/repository_test.dart
@@ -0,0 +1,105 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:motorbike_navigator/data/repository/repository.dart';
+import 'package:motorbike_navigator/entity/entity.dart';
+
+class TestModel extends Entity {
+  final String name;
+
+  const TestModel({required super.id, required this.name});
+
+  @override
+  List<Object> get props => [id, name];
+}
+
+class TestRepository extends Repository<TestModel> {
+  TestRepository({super.initialData});
+}
+
+void main() {
+  late Repository repository;
+
+  setUp(() {
+    repository = TestRepository();
+  });
+
+  test(
+    'initial state should be empty list',
+    () async {
+      repository = TestRepository();
+
+      final state = await repository.repositoryState$.first;
+
+      expect(state, []);
+    },
+  );
+
+  test(
+    'isRepositoryStateEmpty, '
+    'should return true if repository state is empty array',
+    () {
+      repository = TestRepository();
+
+      expect(repository.isRepositoryStateEmpty, true);
+    },
+  );
+
+  test(
+    'isRepositoryStateEmpty, '
+    'should return false if repository state is not empty array',
+    () {
+      repository = TestRepository(
+        initialData: [
+          const TestModel(id: 'id', name: 'name'),
+        ],
+      );
+
+      expect(repository.isRepositoryStateEmpty, false);
+    },
+  );
+
+  test(
+    'addEntity, '
+    'entity with the same id already exists in state, '
+    'should throw exception',
+    () {
+      const TestModel newEntity = TestModel(id: 'e1', name: 'entity 1');
+      final List<TestModel> existingEntities = [
+        const TestModel(id: 'e1', name: 'first entity'),
+        const TestModel(id: 'e2', name: 'second entity'),
+      ];
+      final String expectedException =
+          '[Repository] Entity $newEntity already exists in repository state';
+      repository = TestRepository(initialData: existingEntities);
+
+      Object? exception;
+      try {
+        repository.addEntity(newEntity);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception, expectedException);
+    },
+  );
+
+  test(
+    'addEntity, '
+    'should add new entity to state',
+    () {
+      const TestModel newEntity = TestModel(id: 'e3', name: 'entity 3');
+      final List<TestModel> existingEntities = [
+        const TestModel(id: 'e1', name: 'first entity'),
+        const TestModel(id: 'e2', name: 'second entity'),
+      ];
+      final List<TestModel> expectedEntities = [
+        ...existingEntities,
+        newEntity,
+      ];
+      repository = TestRepository(initialData: existingEntities);
+
+      repository.addEntity(newEntity);
+
+      expect(repository.repositoryState$, emits(expectedEntities));
+    },
+  );
+}
