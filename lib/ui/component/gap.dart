c8c1e1c9c543944dccfe5200ea6e88a530a23492
Wojtas399 <wojtekp26@gmail.com>
2024-06-17 15:52:44 +0200

    Initial UI design of route content.

--- lib/ui/component/gap.dart
+++ lib/ui/component/gap.dart
@@ -15,6 +15,10 @@ class GapVertical24 extends _Gap {
   const GapVertical24() : super(vertical: 24);
 }
 
+class GapVertical16 extends _Gap {
+  const GapVertical16() : super(vertical: 16);
+}
+
 class GapVertical8 extends _Gap {
   const GapVertical8() : super(vertical: 8);
 }
--- lib/ui/map/map_route_content.dart
+++ lib/ui/map/map_route_content.dart
@@ -1,14 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../component/gap.dart';
+import '../extensions/context_extensions.dart';
+import 'cubit/map_cubit.dart';
+import 'cubit/map_state.dart';
 
 class MapRouteContent extends StatelessWidget {
   const MapRouteContent({super.key});
 
   @override
+  Widget build(BuildContext context) => const SafeArea(
+        child: Column(
+          children: [
+            GapVertical24(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _BackButton(),
+                Expanded(
+                  child: Row(
+                    children: [
+                      _RouteIcons(),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: _Form(),
+                      ),
+                      SizedBox(width: 8),
+                      _SwapPointsButton(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+}
+
+class _BackButton extends StatelessWidget {
+  const _BackButton();
+
+  void _onPressed(BuildContext context) {
+    context.read<MapCubit>().changeMode(MapMode.map);
+  }
+
+  @override
+  Widget build(BuildContext context) => IconButton(
+        onPressed: () => _onPressed(context),
+        icon: const Icon(Icons.arrow_back_ios_new),
+      );
+}
+
+class _RouteIcons extends StatelessWidget {
+  const _RouteIcons();
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          Icon(
+            Icons.my_location,
+            color: context.colorScheme.primary,
+          ),
+          const GapVertical16(),
+          const Icon(Icons.more_vert),
+          const GapVertical16(),
+          const Icon(
+            Icons.location_on_outlined,
+            color: Colors.red,
+          ),
+        ],
+      );
+}
+
+class _Form extends StatelessWidget {
+  const _Form();
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          _RouteTextField(
+            hintText: 'Wybierz miejsce startowe',
+          ),
+          GapVertical24(),
+          _RouteTextField(
+            hintText: 'Wybierz miejsce docelowe',
+          ),
+        ],
+      );
+}
+
+class _RouteTextField extends StatelessWidget {
+  final String? hintText;
+
+  const _RouteTextField({
+    this.hintText,
+  });
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Route content'),
+    return TextField(
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        hintText: hintText,
       ),
     );
   }
+}
+
+class _SwapPointsButton extends StatelessWidget {
+  const _SwapPointsButton();
+
+  void _onPressed() {
+    //TODO
+  }
+
+  @override
+  Widget build(BuildContext context) => IconButton(
+        onPressed: () => _onPressed(),
+        icon: const Icon(
+          Icons.swap_vert,
+          size: 32,
+        ),
+      );
 }
