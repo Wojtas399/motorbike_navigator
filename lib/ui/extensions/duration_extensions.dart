9d896774a7529de259dfab6da072708581d27f69
Wojtas399 <wojtekp26@gmail.com>
2024-06-27 12:24:07 +0200

    Created extension method for Duration model to format duration to UI.

---
+++ lib/ui/extensions/duration_extensions.dart
@@ -0,0 +1,8 @@
+extension DurationExtensions on Duration {
+  String toUIFormat() {
+    final int minutes = inMinutes % 60;
+    final int seconds = inSeconds % 60;
+
+    return '$inHours h $minutes min $seconds s';
+  }
+}
