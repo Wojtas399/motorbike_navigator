fbc76ad190bc5075c3639012a7c751fbec182254
Wojtas399 <wojtekp26@gmail.com>
2024-06-28 08:44:06 +0200

    Created confirmation dialog component.

---
+++ lib/ui/component/confirmation_dialog_component.dart
@@ -0,0 +1,41 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:motorbike_navigator/ui/component/text.dart';
+
+import '../extensions/context_extensions.dart';
+
+class ConfirmationDialogComponent extends StatelessWidget {
+  final String title;
+  final String message;
+
+  const ConfirmationDialogComponent({
+    super.key,
+    required this.title,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) => AlertDialog(
+        title: Text(title),
+        content: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 600),
+          child: Text(message),
+        ),
+        actions: [
+          SizedBox(
+            width: 120,
+            child: OutlinedButton(
+              onPressed: () => context.maybePop(false),
+              child: LabelMedium(context.str.cancel),
+            ),
+          ),
+          SizedBox(
+            width: 120,
+            child: FilledButton(
+              onPressed: () => context.maybePop(true),
+              child: Text(context.str.confirm),
+            ),
+          ),
+        ],
+      );
+}
