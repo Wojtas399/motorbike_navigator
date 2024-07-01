8aa8661aab56f8921ae9066d253e06a6728bb211
Wojtas399 <wojtekp26@gmail.com>
2024-07-01 17:40:19 +0200

    Created DriveDto model.

---
+++ lib/data/dto/drive_dto.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'drive_dto.freezed.dart';
+part 'drive_dto.g.dart';
+
+@freezed
+class DriveDto with _$DriveDto {
+  const factory DriveDto({
+    @JsonKey(includeFromJson: false, includeToJson: false)
+    @Default('')
+    String id,
+    required double distanceInKm,
+    required int durationInSeconds,
+    required double avgSpeedInKmPerH,
+    required List<List<double>> waypoints,
+  }) = _DriveDto;
+
+  factory DriveDto.fromJson(Map<String, Object?> json) =>
+      _$DriveDtoFromJson(json);
+
+  factory DriveDto.fromIdAndJson(String id, Map<String, Object?> json) =>
+      DriveDto.fromJson(json).copyWith(id: id);
+}
