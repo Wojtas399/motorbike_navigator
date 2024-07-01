7b58faa8d218048f459174dd7722ea2bf5cf1944
Wojtas399 <wojtekp26@gmail.com>
2024-07-01 12:57:13 +0200

    Created basic SignIn screen UI with button to sign in with google.

--- lib/ui/screen/sign_in/sign_in_screen.dart
+++ lib/ui/screen/sign_in/sign_in_screen.dart
@@ -1,5 +1,10 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../component/gap.dart';
+import '../../component/text.dart';
+import '../../extensions/context_extensions.dart';
 
 @RoutePage()
 class SignInScreen extends StatelessWidget {
@@ -7,8 +12,34 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Logowanie'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TitleLarge(context.str.signInTitle),
+              const GapVertical24(),
+              const _GoogleSignInButton(),
+            ],
+          ),
         ),
+      );
+}
+
+class _GoogleSignInButton extends StatelessWidget {
+  const _GoogleSignInButton();
+
+  void _onPressed(BuildContext context) {
+    //TODO
+  }
+
+  @override
+  Widget build(BuildContext context) => FilledButton.icon(
+        onPressed: () => _onPressed(context),
+        icon: SvgPicture.asset(
+          width: 24,
+          height: 24,
+          'assets/google_logo.svg',
+        ),
+        label: Text(context.str.signInWithGoogleLabel),
       );
 }
