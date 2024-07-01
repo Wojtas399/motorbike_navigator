2192c2e8fd9a31247f649915cc9169bbcfd2d52c
Wojtas399 <wojtekp26@gmail.com>
2024-07-01 18:16:21 +0200

    Created Drive model's mapper.

---
+++ lib/data/mapper/drive_mapper.dart
@@ -0,0 +1,34 @@
+import '../../entity/coordinates.dart';
+import '../../entity/drive.dart';
+import '../dto/coordinates_dto.dart';
+import '../dto/drive_dto.dart';
+
+Drive mapDriveFromDto(DriveDto driveDto) => Drive(
+      id: driveDto.id,
+      distanceInKm: driveDto.distanceInKm,
+      durationInSeconds: driveDto.durationInSeconds,
+      avgSpeedInKmPerH: driveDto.avgSpeedInKmPerH,
+      waypoints: driveDto.waypoints
+          .map(
+            (coordinates) => Coordinates(
+              coordinates.lat,
+              coordinates.long,
+            ),
+          )
+          .toList(),
+    );
+
+DriveDto mapDriveToDto(Drive drive) => DriveDto(
+      id: drive.id,
+      distanceInKm: drive.distanceInKm,
+      durationInSeconds: drive.durationInSeconds,
+      avgSpeedInKmPerH: drive.avgSpeedInKmPerH,
+      waypoints: drive.waypoints
+          .map(
+            (coordinates) => CoordinatesDto(
+              lat: coordinates.latitude,
+              long: coordinates.longitude,
+            ),
+          )
+          .toList(),
+    );
---
+++ test/data/mapper/drive_mapper_test.dart
@@ -0,0 +1,71 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:motorbike_navigator/data/dto/coordinates_dto.dart';
+import 'package:motorbike_navigator/data/dto/drive_dto.dart';
+import 'package:motorbike_navigator/data/mapper/drive_mapper.dart';
+import 'package:motorbike_navigator/entity/coordinates.dart';
+import 'package:motorbike_navigator/entity/drive.dart';
+
+void main() {
+  const String id = 'd1';
+  const double distanceInKm = 10.21;
+  const int durationInSeconds = 50000;
+  const double avgSpeedInKmPerH = 15;
+  const List<Coordinates> waypoints = [
+    Coordinates(50, 18),
+    Coordinates(51, 19),
+  ];
+  const List<CoordinatesDto> waypointsDto = [
+    CoordinatesDto(lat: 50, long: 18),
+    CoordinatesDto(lat: 51, long: 19),
+  ];
+
+  test(
+    'mapDriveFromDto, '
+    'should map DriveDto model to Drive model',
+    () {
+      const DriveDto driveDto = DriveDto(
+        id: id,
+        distanceInKm: distanceInKm,
+        durationInSeconds: durationInSeconds,
+        avgSpeedInKmPerH: avgSpeedInKmPerH,
+        waypoints: waypointsDto,
+      );
+      const Drive expectedDrive = Drive(
+        id: id,
+        distanceInKm: distanceInKm,
+        durationInSeconds: durationInSeconds,
+        avgSpeedInKmPerH: avgSpeedInKmPerH,
+        waypoints: waypoints,
+      );
+
+      final Drive drive = mapDriveFromDto(driveDto);
+
+      expect(drive, expectedDrive);
+    },
+  );
+
+  test(
+    'mapDriveToDto, '
+    'should map Drive model to DriveDto model',
+    () {
+      const Drive drive = Drive(
+        id: id,
+        distanceInKm: distanceInKm,
+        durationInSeconds: durationInSeconds,
+        avgSpeedInKmPerH: avgSpeedInKmPerH,
+        waypoints: waypoints,
+      );
+      const DriveDto expectedDriveDto = DriveDto(
+        id: id,
+        distanceInKm: distanceInKm,
+        durationInSeconds: durationInSeconds,
+        avgSpeedInKmPerH: avgSpeedInKmPerH,
+        waypoints: waypointsDto,
+      );
+
+      final DriveDto driveDto = mapDriveToDto(drive);
+
+      expect(driveDto, expectedDriveDto);
+    },
+  );
+}
