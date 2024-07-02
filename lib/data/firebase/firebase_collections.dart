a10b8354bb6a51db986156a2106ed9a825754190
Wojtas399 <wojtekp26@gmail.com>
2024-07-02 14:25:54 +0200

    Created FirebaseUserService.

--- lib/data/firebase/firebase_collections.dart
+++ lib/data/firebase/firebase_collections.dart
@@ -1,6 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../dto/drive_dto.dart';
+import '../dto/user_dto.dart';
+
+CollectionReference<UserDto> getUsersRef() =>
+    FirebaseFirestore.instance.collection('Users').withConverter<UserDto>(
+          fromFirestore: (snapshot, _) {
+            final data = snapshot.data();
+            if (data == null) throw 'User document data was null';
+            return UserDto.fromIdAndJson(snapshot.id, data);
+          },
+          toFirestore: (UserDto dto, _) => dto.toJson(),
+        );
 
 CollectionReference<DriveDto> getDrivesRef(
   String userId,
---
+++ lib/data/firebase/firebase_user_service.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:injectable/injectable.dart';
+
+import '../dto/user_dto.dart';
+import 'firebase_collections.dart';
+
+@injectable
+class FirebaseUserService {
+  Future<UserDto?> fetchUserById({required String userId}) async {
+    final snapshot = await getUsersRef().doc(userId).get();
+    return snapshot.data();
+  }
+
+  Future<UserDto?> addUser({
+    required String userId,
+    required ThemeModeDto themeMode,
+  }) async {
+    final docRef = getUsersRef().doc(userId);
+    await docRef.set(
+      UserDto(
+        themeMode: themeMode,
+      ),
+    );
+    final snapshot = await docRef.get();
+    return snapshot.data();
+  }
+
+  Future<UserDto?> updateUserThemeMode({
+    required String userId,
+    required ThemeModeDto themeMode,
+  }) async {
+    final docRef = getUsersRef().doc(userId);
+    DocumentSnapshot<UserDto> doc = await docRef.get();
+    UserDto? data = doc.data();
+    if (data == null) {
+      throw '[FirebaseUserService] Cannot find doc data';
+    }
+    data = data.copyWith(
+      themeMode: themeMode,
+    );
+    await docRef.set(data);
+    final snapshot = await docRef.get();
+    return snapshot.data();
+  }
+}
