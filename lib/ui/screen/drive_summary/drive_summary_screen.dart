8000321b7ef29c65e90252b0e2bab824b4fd617b
Wojtas399 <wojtekp26@gmail.com>
2024-07-09 13:07:30 +0200

    Transformed DialogService into class.

--- lib/ui/screen/drive_summary/drive_summary_screen.dart
+++ lib/ui/screen/drive_summary/drive_summary_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../dependency_injection.dart';
 import '../../component/gap.dart';
 import '../../cubit/drive/drive_cubit.dart';
 import '../../extensions/context_extensions.dart';
@@ -40,10 +41,11 @@ class _DriveActions extends StatelessWidget {
   const _DriveActions();
 
   Future<void> _onDelete(BuildContext context) async {
-    final bool deleteConfirmation = await askForConfirmation(
-      title: context.str.driveDeleteConfirmationTitle,
-      message: context.str.driveDeleteConfirmationMessage,
-    );
+    final bool deleteConfirmation =
+        await getIt.get<DialogService>().askForConfirmation(
+              title: context.str.driveDeleteConfirmationTitle,
+              message: context.str.driveDeleteConfirmationMessage,
+            );
     if (deleteConfirmation == true && context.mounted) {
       await context.maybePop();
       if (context.mounted) context.read<DriveCubit>().resetDrive();
--- lib/ui/service/dialog_service.dart
+++ lib/ui/service/dialog_service.dart
@@ -1,40 +1,45 @@
 import 'package:flutter/widgets.dart';
+import 'package:injectable/injectable.dart';
 
 import '../../dependency_injection.dart';
 import '../component/confirmation_dialog_component.dart';
 import '../config/app_router.dart';
 
-Future<bool> askForConfirmation({
-  required String title,
-  required String message,
-}) async =>
-    await showAlertDialog(
-      ConfirmationDialogComponent(
-        title: title,
-        message: message,
-      ),
-      barrierDismissible: false,
-    ) ==
-    true;
+@injectable
+class DialogService {
+  Future<bool> askForConfirmation({
+    required String title,
+    required String message,
+  }) async =>
+      await _showAlertDialog(
+        ConfirmationDialogComponent(
+          title: title,
+          message: message,
+        ),
+        barrierDismissible: false,
+      ) ==
+      true;
 
-Future<T?> showAlertDialog<T>(
-  Widget dialog, {
-  bool barrierDismissible = true,
-}) async {
-  final BuildContext? context = getIt<AppRouter>().navigatorKey.currentContext;
-  if (context == null) return null;
-  return await showGeneralDialog<T>(
-    context: context,
-    pageBuilder: (_, anim1, anim2) => dialog,
-    transitionBuilder: (BuildContext context, anim1, anim2, child) {
-      var curve = Curves.easeInOutQuart.transform(anim1.value);
-      return Transform.scale(
-        scale: curve,
-        child: child,
-      );
-    },
-    transitionDuration: const Duration(milliseconds: 250),
-    barrierDismissible: barrierDismissible,
-    barrierLabel: '',
-  );
+  Future<T?> _showAlertDialog<T>(
+    Widget dialog, {
+    bool barrierDismissible = true,
+  }) async {
+    final BuildContext? context =
+        getIt<AppRouter>().navigatorKey.currentContext;
+    if (context == null) return null;
+    return await showGeneralDialog<T>(
+      context: context,
+      pageBuilder: (_, anim1, anim2) => dialog,
+      transitionBuilder: (BuildContext context, anim1, anim2, child) {
+        var curve = Curves.easeInOutQuart.transform(anim1.value);
+        return Transform.scale(
+          scale: curve,
+          child: child,
+        );
+      },
+      transitionDuration: const Duration(milliseconds: 250),
+      barrierDismissible: barrierDismissible,
+      barrierLabel: '',
+    );
+  }
 }
