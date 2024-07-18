f92ee27955c60a27a50a1687cce8d98694d3e900
Wojtas399 <wojtekp26@gmail.com>
2024-07-18 16:48:33 +0200

    Added tests for copyWith method of SavedDrivesState.

--- test/ui/screen/saved_drives/saved_drives_state_test.dart
+++ test/ui/screen/saved_drives/saved_drives_state_test.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:motorbike_navigator/entity/drive.dart';
 import 'package:motorbike_navigator/ui/screen/saved_drives/cubit/saved_drives_state.dart';
+
+import '../../../creator/drive_creator.dart';
 
 void main() {
   test(
@@ -13,6 +16,61 @@ void main() {
       const state = SavedDrivesState();
 
       expect(state, expectedDefaultState);
+    },
+  );
+
+  group(
+    'copyWith status, ',
+    () {
+      const expectedStatus = SavedDrivesStateStatus.completed;
+      SavedDrivesState state = const SavedDrivesState();
+
+      test(
+        'should update status if new value has been passed, ',
+        () {
+          state = state.copyWith(status: expectedStatus);
+
+          expect(state.status, expectedStatus);
+        },
+      );
+
+      test(
+        'should copy current value if new value has not been passed',
+        () {
+          state = state.copyWith();
+
+          expect(state.status, expectedStatus);
+        },
+      );
+    },
+  );
+
+  group(
+    'copyWith drives, ',
+    () {
+      final driveCreator = DriveCreator();
+      final expectedDrives = <Drive>[
+        driveCreator.create(id: 'd1'),
+      ];
+      SavedDrivesState state = const SavedDrivesState();
+
+      test(
+        'should update drives if new value has been passed, ',
+        () {
+          state = state.copyWith(drives: expectedDrives);
+
+          expect(state.drives, expectedDrives);
+        },
+      );
+
+      test(
+        'should copy current value if new value has not been passed',
+        () {
+          state = state.copyWith();
+
+          expect(state.drives, expectedDrives);
+        },
+      );
     },
   );
 }
