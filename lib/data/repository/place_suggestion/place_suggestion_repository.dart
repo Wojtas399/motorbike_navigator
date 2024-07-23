e8cf1eedfec3c1d837d5297377ef037e0e89adf3
Wojtas399 <wojtekp26@gmail.com>
2024-07-23 12:16:39 +0200

    Deleted PlaceSuggestion repository.

--- lib/data/repository/place_suggestion/place_suggestion_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../../../entity/place_suggestion.dart';
-
-abstract interface class PlaceSuggestionRepository {
-  Future<List<PlaceSuggestion>> searchPlaces({
-    required String query,
-    required int limit,
-  });
-}
