01195d1f8f9308666708de0302bc46b29f9f5ca3
Wojtas399 <wojtekp26@gmail.com>
2024-07-23 12:01:10 +0200

    Deleted RouteSuggestions repository.

--- lib/data/repository/route_suggestions/route_suggestions_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../../entity/coordinates.dart';
-import '../../../entity/route_suggestions.dart';
-
-abstract interface class RouteSuggestionsRepository {
-  Future<RouteSuggestions?> loadRouteSuggestionsByStartAndEndLocations({
-    required Coordinates startLocation,
-    required Coordinates endLocation,
-  });
-}
