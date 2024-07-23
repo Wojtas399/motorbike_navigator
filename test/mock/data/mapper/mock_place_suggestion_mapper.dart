c0de8af3304cab0ab111520907251378d51c9684
Wojtas399 <wojtekp26@gmail.com>
2024-07-23 12:17:22 +0200

    Deleted PlaceSuggestion mapper.

--- test/mock/data/mapper/mock_place_suggestion_mapper.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:motorbike_navigator/data/mapper/place_suggestion_mapper.dart';
-
-class MockPlaceSuggestionMapper extends Mock implements PlaceSuggestionMapper {}
