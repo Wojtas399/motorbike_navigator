1945837cc585b957191af1406681375f41e1723e
Wojtas399 <wojtekp26@gmail.com>
2024-07-23 11:58:18 +0200

    Removed using of RouteCubit by MapMarkerLayer.

--- lib/ui/screen/map/map_marker_layer.dart
+++ lib/ui/screen/map/map_marker_layer.dart
@@ -3,9 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 
 import '../../../entity/coordinates.dart';
-import '../../component/end_route_icon_component.dart';
-import '../../component/start_route_icon_component.dart';
-import '../../cubit/route/route_cubit.dart';
 import '../../extensions/coordinates_extensions.dart';
 import 'cubit/map_cubit.dart';
 
@@ -16,9 +13,6 @@ class MapMarkerLayer extends StatelessWidget {
   Widget build(BuildContext context) {
     final Coordinates? userLocation = context.select(
       (MapCubit cubit) => cubit.state.userLocation,
-    );
-    final List<Coordinates>? routeWaypoints = context.select(
-      (RouteCubit cubit) => cubit.state.route?.waypoints,
     );
 
     return MarkerLayer(
@@ -36,16 +30,6 @@ class MapMarkerLayer extends StatelessWidget {
               ),
             ),
           ),
-        if (routeWaypoints?.isNotEmpty == true) ...[
-          Marker(
-            point: routeWaypoints!.first.toLatLng(),
-            child: const StartRouteIcon(),
-          ),
-          Marker(
-            point: routeWaypoints.last.toLatLng(),
-            child: const EndRouteIcon(),
-          ),
-        ],
       ],
     );
   }
