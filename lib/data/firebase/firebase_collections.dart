ebf055cae8368f85a10831c3a449b7aa7afe85ef
Wojtas399 <wojtekp26@gmail.com>
2024-08-18 17:36:09 +0200

    Deleted unused DriveDto model and FirebaseDriveService.

--- lib/data/firebase/firebase_collections.dart
+++ lib/data/firebase/firebase_collections.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import '../dto/drive_dto.dart';
 import '../dto/user_dto.dart';
 
 CollectionReference<UserDto> getUsersRef() =>
@@ -14,24 +13,4 @@ CollectionReference<UserDto> getUsersRef() =>
             );
           },
           toFirestore: (UserDto dto, _) => dto.toJson(),
-        );
-
-CollectionReference<DriveDto> getDrivesRef(
-  String userId,
-) =>
-    FirebaseFirestore.instance
-        .collection('Users')
-        .doc(userId)
-        .collection('Drives')
-        .withConverter<DriveDto>(
-          fromFirestore: (snapshot, _) {
-            final data = snapshot.data();
-            if (data == null) throw 'Drive document data was null';
-            return DriveDto.fromFirebaseFirestore(
-              driveId: snapshot.id,
-              userId: userId,
-              json: data,
-            );
-          },
-          toFirestore: (DriveDto dto, _) => dto.toJson(),
         );
