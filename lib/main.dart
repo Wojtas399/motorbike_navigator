c275b5faa44e136a1af239062b16b97471517fc6
Wojtas399 <wojtekp26@gmail.com>
2024-09-03 14:26:32 +0200

    Removed initializing Firebase in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mapbox_search/mapbox_search.dart';
 
 import 'app.dart';
 import 'dependency_injection.dart';
 import 'env.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   configureDependencies();
   MapBoxSearch.init(Env.mapboxAccessToken);
   runApp(const App());
