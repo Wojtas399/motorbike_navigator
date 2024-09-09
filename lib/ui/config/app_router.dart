3d5a79969ef06c2669a25c378bf5a8147439318a
Wojtas399 <wojtekp26@gmail.com>
2024-09-09 18:06:17 +0200

    Resolved analyzer warnings.

--- lib/ui/config/app_router.dart
+++ lib/ui/config/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:injectable/injectable.dart';
 
-import '../../entity/drive.dart';
 import '../screen/screens.dart';
 
 part 'app_router.gr.dart';
