7e2fe80b7be88b0973b12cd737e2fb18c7d8f3c1
Wojtas399 <wojtekp26@gmail.com>
2024-09-21 17:47:09 +0200

    Removed unused packages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:mapbox_search/mapbox_search.dart';
 
 import 'app.dart';
 import 'dependency_injection.dart';
-import 'env.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-  MapBoxSearch.init(Env.mapboxAccessToken);
   runApp(const App());
 }
